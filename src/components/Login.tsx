e75c7fcbe10d5681e3d5618d429254f91a6a12f0
Jon Jackson <ocskier@gmail.com>
2021-11-24 00:35:22 -0500

    feat: adds login dispatch, persistence with click, navigate - closes #5

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,3 +1,25 @@
+import { useNavigate } from 'react-router-dom';
+
+import { useGlobalContext } from '../context/Store';
+import { types } from '../context/actions';
+
+import { updateUserPersistence } from '../utils/localStorage';
+
 export const Login = () => {
-  return <div>Login Form</div>;
+  const { dispatch } = useGlobalContext();
+  const navigate = useNavigate();
+  const login = () => {
+    const newUser = {
+      username: 'tester',
+      password: 'password',
+      permissions: 'admin',
+    };
+    dispatch({
+      type: types.LOGIN,
+      payload: newUser,
+    });
+    updateUserPersistence(newUser);
+    navigate('/dashboard');
+  };
+  return <div onClick={login}>Login Form</div>;
 };
