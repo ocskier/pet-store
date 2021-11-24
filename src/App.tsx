e62e49fb5e92386c0c650c9082fd93c3015fa128
Jon Jackson <ocskier@gmail.com>
2021-11-23 23:58:13 -0500

    feat: adds working authorization routes for three views - closes #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import { ProtectedRoute, PublicRoute } from './routes/Routes';
 
 import { useGlobalContext } from './context/Store';
 import { types } from './context/actions';
@@ -12,14 +15,45 @@ const App = () => {
     state: { loggedIn },
     dispatch,
   } = useGlobalContext();
-  console.log(loggedIn);
+
   useEffect(() => {
-    updateUserPersistence({ username: 'test', password: 'password', permissions: 'customer' }); // testing loggedIn
+    // updateUserPersistence({ username: 'test', password: 'password', permissions: 'customer' }); // testing loggedIn
     const user = readUserPersistence();
     Object.keys(user).length && dispatch({ type: types.ME, payload: user });
   }, [dispatch]);
 
-  return <div className="App">{String(loggedIn)}</div>;
+  return (
+    <div className="App">
+      <main>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <PublicRoute>
+                <p>Home Page</p>
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <PublicRoute auth={loggedIn} restricted={true}>
+                <p>Login View</p>
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute auth={loggedIn}>
+                <p>Dashboard</p>
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </main>
+    </div>
+  );
 };
 
 export default App;
