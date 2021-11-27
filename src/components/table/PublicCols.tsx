d473e98d38daf48ba99b9b3cc2993920ac7f04ff
Jon Jackson <ocskier@gmail.com>
2021-11-26 21:49:48 -0500

    feat: adds tailored last status col by permission

---
+++ src/components/table/PublicCols.tsx
@@ -0,0 +1,17 @@
+import { GridColDef } from '@mui/x-data-grid';
+import { Typography } from '@mui/material';
+
+import { Pet } from '../../types/globalTypes';
+
+export const PublicCols: GridColDef[] = [
+  {
+    field: 'status',
+    headerName: 'Status',
+    align: 'center',
+    headerAlign: 'center',
+    flex: 1,
+    editable: false,
+    disableColumnMenu: true,
+    renderCell: ({ row }: { row: Pet }) => <Typography>{row.status[0] + row.status.slice(1)}</Typography>,
+  },
+];
