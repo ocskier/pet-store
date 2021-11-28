ffa36797708c404a72f5195b49b1ac7e51c86e42
Jon Jackson <ocskier@gmail.com>
2021-11-28 13:18:48 -0500

    docs: adds context comments

--- src/context/Store.tsx
+++ src/context/Store.tsx
@@ -1,24 +1,33 @@
+//  React hook imports
 import { createContext, FC, useContext, useReducer } from 'react';
 
+// Matching reducer import
 import reducer from './reducer';
 
+// Type and interface imports
 import { GlobalContextInterface, ProvProps } from '../types/globalTypes';
 
+// Create App initial global  state value (no pets or logged in user)
 const initialState = {
   loggedIn: false,
   user: null,
   pets: [],
 };
 
+// Create a global context with initial state and empty dispatch
 const GlobalContext = createContext<GlobalContextInterface>({
   state: initialState,
   dispatch: () => {},
 });
 
+// Destructure global Provider from context
 const { Provider } = GlobalContext;
 
+// Custom hook for accessing the global state around the app
 export const useGlobalContext = () => useContext(GlobalContext);
 
+// Custom Provider component to wrap around app with initial state and
+// defined dispatch
 export const GlobalProvider: FC<ProvProps> = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
   return <Provider value={{ state, dispatch }}>{children}</Provider>;
--- src/context/actions.ts
+++ src/context/actions.ts
@@ -1,6 +1,24 @@
+// Action types
 export const types = {
+  //  LOGIN:
+  //  takes a user object as payload with username and password parameters
+  //  updates user state value
+  //  changes auth state loggedIn to true
   LOGIN: 'LOGIN',
+
+  //  LOGOUT:
+  //  updates user state to null
+  //  changes auth state loggedIn to false
   LOGOUT: 'LOGOUT',
+
+  //  ME:
+  //  takes a user profile from ls persistence as payload
+  //  updates user state value
+  //  changes auth state loggedIn to true
   ME: 'ME',
+
+  //  SET_PETS:
+  //  takes a new pets array as payload
+  //  updates pets state value with new array
   SET_PETS: 'SET_PETS',
 };
