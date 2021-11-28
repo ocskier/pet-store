59854d0022da3b53995f9a1836754ec37af9af4b
Jon Jackson <ocskier@gmail.com>
2021-11-28 13:28:16 -0500

    docs: adds route and types comments

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,15 +1,20 @@
+// Dependency imports
 import { Navigate, RouteProps } from 'react-router-dom';
 
+// Custom route props interface extending React props to allow for auth, restricted,
+// and children prop values to be passed in
 interface CustomRouteProps extends RouteProps {
   auth?: boolean;
   children: JSX.Element;
   restricted?: boolean;
 }
 
+// Custom route that if authenticated shows the component else reroutes to login
 export const ProtectedRoute = ({ auth, children }: CustomRouteProps) => {
   return <>{auth ? children : <Navigate to="/login" />}</>;
 };
 
+// Custom route for all however if /login and user authenticted reroutes to Dashboard
 export const PublicRoute = ({ auth, children, restricted }: CustomRouteProps) => {
   return <>{auth && restricted ? <Navigate to={'/dashboard'} /> : children}</>;
 };
--- src/types/globalTypes.ts
+++ src/types/globalTypes.ts
@@ -1,5 +1,7 @@
+//  React type imports
 import { Dispatch } from 'react';
 
+// Custom type for a Pet object
 export type Pet = {
   id: number;
   category: string;
@@ -9,28 +11,34 @@ export type Pet = {
   status: 'available' | 'pending' | 'sold';
 };
 
+// Custom type for a User object
 export type User = {
   username: string;
   password: string;
   permissions: 'admin' | 'customer';
 };
 
+// Custom interface for a Dispatch Action
 export interface Action {
   type: string;
   payload?: any;
 }
 
+// Custom interface for a Global State object
 export interface State {
   loggedIn: boolean;
   user: User | null;
   pets: Pet[];
 }
 
+// Custom interface for a Global Context value object
 export interface GlobalContextInterface {
   state: State;
   dispatch: Dispatch<Action>;
 }
 
+// Custom interface for Global Provider props to allow
+// for children elements to be rendered
 export interface ProvProps {
   children: JSX.Element;
 }
