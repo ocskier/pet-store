6e3e25119b03a137349fe7ff9c473d35f751c9a2
Jon Jackson <ocskier@gmail.com>
2021-11-29 16:06:46 -0500

    hotfix: removes roboto package in favor of cdn

--- src/index.tsx
+++ src/index.tsx
@@ -12,11 +12,6 @@ import { GlobalProvider } from './context/Store';
 // Style imports
 import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
-import '@fontsource/roboto/300.css';
-// issues with roboto font loading
-// import '@fontsource/roboto/400.css';
-// import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 
 // Analytics import
 import reportWebVitals from './reportWebVitals';
