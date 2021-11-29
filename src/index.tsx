4b9e409c43fb484c322d3edec46cb3be6919dfec
Jon Jackson <36890724+ocskier@users.noreply.github.com>
2021-11-29 13:15:04 -0500

    Adds comments to index file
    Closes #70

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,22 @@
+//  React imports
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+// App import
 import App from './App';
+
+// Global State imports
 import { GlobalProvider } from './context/Store';
 
+// Style imports
 import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
 
+// Analytics import
 import reportWebVitals from './reportWebVitals';
 
+// Wrapping our app with the global state and browser client-side router
 ReactDOM.render(
   <StrictMode>
     <GlobalProvider>
